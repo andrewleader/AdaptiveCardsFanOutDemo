72ec739ac8d06e340f1c95f3fdff51e53a83ceb4
Andrew Bares <anbare@microsoft.com>
2018-05-03 16:37:26 -0700

    Mothership UI improvements
    Timing so that it doesn't feel clients are delayed, and new cards come in from the top

--- VSSolution/MothershipApp/MainPage.xaml.cs
+++ VSSolution/MothershipApp/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using MothershipApp.ViewModels;
+﻿using FanOutUwpClassLibrary.ViewModels;
+using MothershipApp.ViewModels;
+using MothershipApp.Views;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -6,12 +8,14 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
